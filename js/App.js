07c707de6c9474407f087a3e9a00dd6dfb719bd6
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-01-18 18:19:25 -0300

    start to create a system who do simples calc

--- js/App.js
+++ js/App.js
@@ -5,98 +5,116 @@ class Calculator{
         this.operation = ""
         this.finalResult = "a"
         this.operators = {
-            "+": (n1, n2)=>n1+n2,
-            "-": (n1, n2)=>n1-n2,
-            "*": (n1, n2)=>n1*n2,
-            "/": (n1, n2)=>n1/n2,
-            "(": "priority", 
-            ")": "priority"
+            "+": { solve: (n1, n2)=>n1+n2, priority: 0 },
+            "-": { solve: (n1, n2)=>n1-n2, priority: 0 },
+            "*": { solve: (n1, n2)=>n1*n2, priority: 1 },
+            "/": { solve: (n1, n2)=>n1/n2, priority: 1 },
+            "(": { priority: 2 },
+            ")": { priority: 2 },
+            " ": { priority: null} // Show the end of the operation
         } 
     }
 
-    set setFinalResult(r){
-        this.finalResult = r
-    }
-
     get solveTheOperation(){
-        let orderToSolve = new Map()
-        let currentResult = null
-        
-        //First function, eliminate the parentheses
-        // How the parentheses has the bigger order to solve, it will show me how many parentheses has
-        function eliminateAllParentheses(expression, operators){
-            let parenthesesCount = 0;
-            let currentExpression = expression
-
-            for (let i of expression)
-                if( i === '(')
-                    parenthesesCount++
-
-            // 10+(60*3)/(2+(100/(100*1)))
-            for(let i = 2; i < parenthesesCount; i++){
-                let toAddRest = false
-                let currentExpressionCache = '' 
-                let secondCache = ''
-                let shortExpression = '' 
-                let foundAParentheses = false
-
-                for(let j of currentExpression){
-                    if(j === '(' && !toAddRest){
-                        shortExpression = ''
-                        foundAParentheses = true
-                        continue;
-
-                    }else if(toAddRest){
-                        currentExpressionCache += j
-
-                    }else if(!foundAParentheses){
-                        currentExpressionCache += j 
-                        continue;
-
-                    }else if(j !== ')'){
-                        shortExpression += j
-                        continue;
-                        
-                    }else{
-                        toAddRest = true
-                        let toSolveIt = {toSolve: ['', ''], operator: '', firstOrSecondNumber: 'first'}
-                        for(let n of shortExpression){
-                            if(operators[n]){
-                                toSolveIt.operator = n
-                                toSolveIt.firstOrSecondNumber = 'second'
-                                continue;
-                            }
-                            
-                            if(toSolveIt.firstOrSecondNumber === 'first')
-                                toSolveIt.toSolve[0] += n
-
-                            else
-                                toSolveIt.toSolve[1] += n
+        // 20+10*3/3
+        function solveASimpleExpression(expression, operators){
+            let hightPriorityOrderCount = 0
+            let normalPriorityOrderCount = 0
+            
+            function solveAllExpressionsWithPriorityOrder(){
+                for(let i of expression){
+                    if(operators[i]){
+                        if(operators[i].priority > 0){
+                            hightPriorityOrderCount++
+                        }else if(operators[i].priority === 0){
+                            normalPriorityOrderCount++
                         }
-                        const [n1, n2] = toSolveIt.toSolve
-                        currentExpressionCache += operators[toSolveIt.operator](Number(n1), Number(n2))
                     }
                 }
-                
-                currentExpression = currentExpressionCache
-                console.log(currentExpression, '\n', currentExpressionCache)
+
+                for(let i = 0; i < hightPriorityOrderCount; i++){
+                    // "20+10/10"
+                    let quickExpressionCache = ''
+                    let finalExpressionCache = ''
+                    let hightPriority = 0
+                    let currentOperator = ''
+                    let toSolve = []
+                    let ended = false
+
+                    for(let j of expression){
+                        if(operators[j] && !ended){
+                            if(hightPriority === 1){
+                                toSolve.push(quickExpressionCache)
+                                finalExpressionCache += operators[currentOperator].solve( Number(toSolve[0]), Number(toSolve[1]))
+                                quickExpressionCache = j
+                                ended = true
+
+                                continue;
+                            }else if(operators[j].priority > 0){
+                                currentOperator = j
+                                toSolve.push(quickExpressionCache)
+                                quickExpressionCache = ''
+
+                                hightPriority += 1
+                                continue;
+
+                            }else if(operators[j].priority === 0){
+                                finalExpressionCache += quickExpressionCache
+                                finalExpressionCache += j
+                                quickExpressionCache = ''
+                            }
+                        }else{
+                            quickExpressionCache += j;
+                        }
+                    }
+                    finalExpressionCache += quickExpressionCache
+                    expression = finalExpressionCache
+                }
             }
 
+            solveAllExpressionsWithPriorityOrder()
+
+            function solveTheRest(){
+                for(let i = 0; i < normalPriorityOrderCount; i++){
+                    let ended = false
+                    let quickExpressionCache = ''
+                    let finalExpressionCache = ''
+                    let currentOperator = ''
+                    let toSolve = []
+
+                    for(let j of expression){
+                        if(operators[j]){
+                            if(ended){
+                                toSolve.push(quickExpressionCache)
+                                console.log(operators[currentOperator].solve(toSolve[0], 3))
+                                console.log(operators[currentOperator].solve(toSolve[0], toSolve[1]))
+                                continue;
+                            }else{
+                                toSolve.push(quickExpressionCache)
+                                quickExpressionCache = ''
+                                currentOperator = j
+                                ended = true
+                            }
+                        }else{
+                            quickExpressionCache += j 
+                        }
+                    }
+                } 
+            }
+
+            solveTheRest()
+
+            console.log(expression)
         }
 
-        eliminateAllParentheses(this.operation, this.operators)
-
-    }
-
-    get getFinalResult(){
-        return this.finalResult
-    }
-
-    set changeOperation(newO){
-        this.operation = newO
+        solveASimpleExpression("10*80+10*4/2*10-100 ", this.operators)
     } 
 }
-// 10+(60*3)/(2+(100/(100*1)))
+
+const calculator = new Calculator()
+calculator.solveTheOperation
+
+// 10+(60*3)/(2+(100/(100*0+100)))
 // 100*1 = 100
 // 10+60*3/(2+(100/100)) 
 // 100/100 = 1
@@ -107,10 +125,4 @@ class Calculator{
 // 10+180/3
 // 10+60 = 70
 // 70
-
-
-const calculator = new Calculator()
-calculator.changeOperation = "10+(60*3)/(2+(100/(100*1)))"
-
-calculator.solveTheOperation
 
