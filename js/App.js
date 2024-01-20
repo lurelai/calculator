c0cd31c5851a68b7722d28a2c07e106b8831261b
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-01-20 20:24:16 -0300

    Finish the system who show the operation in view

--- js/App.js
+++ js/App.js
@@ -7,15 +7,25 @@ const App = {
         try{
             const operation = document.querySelector('.operation')
             const buttons = document.querySelectorAll('.button')
+            const calculator = new Calculator
 
             for(let button of buttons){
                 if(button.attributes.class.value.includes('nothing'))
                     continue;
 
                 button.addEventListener('click', (e)=>{
-                    view.changeOperation(operation, noticeAllButtons(button, operation))
+                    const valueToChange = noticeAllButtons(button, operation)
+
+                    if(valueToChange === 'submit'){
+
+                    }
+
+                    else
+                        view.changeOperation(operation, valueToChange)
                 })
             }
+
+            console.log(calculator)
         }catch(err){
             console.log(err)
         }
--- js/modules/noticeAllButtons.js
+++ js/modules/noticeAllButtons.js
@@ -1,17 +1,51 @@
 export const noticeAllButtons = (button, operation)=>{
     const { innerText: text } = operation
+    const lastLetter = text.at(-1)
 
     const buttonsInfo = {
         numbers: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
         specials: {
-            "clear": ()=>{ return "0" },
             "parentheses": (numbers)=>{
+                const operatorsValues = ['+', '-', '*', '/']
+                let openParenthesesCount = 0
+                let closeParenthesesCount = 0
+
+                for(let i of text){
+                    if(i === '(')
+                        openParenthesesCount++
+
+                    if(i === ')')
+                        closeParenthesesCount++
+                }
+
+                if(lastLetter === ')' && openParenthesesCount - closeParenthesesCount !== 0) 
+                    return text+')'
+
+                if(lastLetter === ')' && openParenthesesCount - closeParenthesesCount === 0)
+                    return text+'X'+'('
+
+                if(operatorsValues.includes(lastLetter))
+                    return text+'('
+
+                if(numbers.includes(lastLetter)){
+                    if(openParenthesesCount - closeParenthesesCount === 0)
+                        return text+'X'+'('
+
+                    else
+                        return text+')'
+                }
+
+                if(lastLetter === '(')
+                    return text+'('
             },
+
             "divide": ()=>{ return text + '/' },
             "times": ()=>{ return text + 'X' },
             "plus": ()=>{ return text + '+' },
             "minus": ()=>{ return text + '-' },
-            "submit": ()=>{ return text + '=' },
+            "submit": ()=>{ 
+                return 'submit' 
+            },
 
             "comma": ()=>{ 
                 const operatorsValues = ['+', '-', '*', '/']
@@ -27,7 +61,6 @@ export const noticeAllButtons = (button, operation)=>{
                     if(operatorsValues.includes(i))
                         operatorCountCache++;                        
                     
-                    console.log(operatorCountCache, operatorCount)
                     if(operatorCountCache === operatorCount)
                         if(i === ',')
                             canAddAComma = false;
@@ -46,16 +79,24 @@ export const noticeAllButtons = (button, operation)=>{
     if(buttonsInfo.numbers.includes(button.innerText)){
         if(text === '0')
             return button.innerText
+        
+        else if(lastLetter === ')')
+            return text
+
         else
             return text + button.innerText
     }
 
     else if(button.id === "parentheses"){
-        console.log(true)
+        return buttonsInfo.specials.parentheses(buttonsInfo.numbers)
+    }
+
+    else if(button.id === "clear"){
+        return '0'
     }
 
     else{
-        if ( buttonsInfo.numbers.includes( text.at(-1) ))
+        if ( buttonsInfo.numbers.includes( lastLetter) || text.at(-1) === ')')
             return buttonsInfo.specials[button.id]()
 
         else
